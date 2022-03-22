a4586f87229eeb43393220fd0b0429f291718202
bot <abagoforgans@users.noreply.github.com>
2022-03-22 08:22:59 +0000

    autocommit

---
+++ contracts/0x1e7131a9Ba09814e436FfdEEA70Dd3AeDFC809C1.sol
@@ -0,0 +1,138 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+mapping of uint256 balanceOf;
+mapping of uint256 allowance;
+uint256 totalSupply;
+array of uint256 name;
+array of uint256 symbol;
+uint8 decimals;
+
+function name() {
+    return name[0 len name.length]
+}
+
+function totalSupply() {
+    return totalSupply
+}
+
+function decimals() {
+    return decimals
+}
+
+function balanceOf(address arg1) {
+    require calldata.size - 4 >= 32
+    return balanceOf[address(arg1)]
+}
+
+function symbol() {
+    return symbol[0 len symbol.length]
+}
+
+function allowance(address arg1, address arg2) {
+    require calldata.size - 4 >= 64
+    return allowance[address(arg1)][address(arg2)]
+}
+
+function _fallback() payable {
+    revert
+}
+
+function approve(address arg1, uint256 arg2) {
+    require calldata.size - 4 >= 64
+    if not msg.sender:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 'ERC20: approve from the zero address'
+    if not arg1:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 'ERC20: approve to the zero address'
+    allowance[address(msg.sender)][address(arg1)] = arg2
+    emit Approval(arg2, msg.sender, arg1);
+    return 1
+}
+
+function burn(uint256 arg1) {
+    require calldata.size - 4 >= 32
+    if not msg.sender:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 'ERC20: burn from the zero address'
+    if arg1 > totalSupply:
+        revert with 0, 'SafeMath: subtraction overflow'
+    totalSupply -= arg1
+    if arg1 > balanceOf[address(msg.sender)]:
+        revert with 0, 'SafeMath: subtraction overflow'
+    balanceOf[address(msg.sender)] -= arg1
+    emit Transfer(arg1, msg.sender, 0);
+}
+
+function decreaseAllowance(address arg1, uint256 arg2) {
+    require calldata.size - 4 >= 64
+    if arg2 > allowance[address(msg.sender)][address(arg1)]:
+        revert with 0, 'SafeMath: subtraction overflow'
+    if not msg.sender:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 'ERC20: approve from the zero address'
+    if not arg1:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 'ERC20: approve to the zero address'
+    allowance[address(msg.sender)][address(arg1)] -= arg2
+    emit Approval((allowance[address(msg.sender)][address(arg1)] - arg2), msg.sender, arg1);
+    return 1
+}
+
+function increaseAllowance(address arg1, uint256 arg2) {
+    require calldata.size - 4 >= 64
+    if allowance[address(msg.sender)][address(arg1)] + arg2 < allowance[address(msg.sender)][address(arg1)]:
+        revert with 0, 'SafeMath: addition overflow'
+    if not msg.sender:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 'ERC20: approve from the zero address'
+    if not arg1:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 'ERC20: approve to the zero address'
+    allowance[address(msg.sender)][address(arg1)] += arg2
+    emit Approval((allowance[address(msg.sender)][address(arg1)] + arg2), msg.sender, arg1);
+    return 1
+}
+
+function transfer(address arg1, uint256 arg2) {
+    require calldata.size - 4 >= 64
+    if not msg.sender:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 'ERC20: transfer from the zero address'
+    if not arg1:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 'ERC20: transfer to the zero address'
+    if arg2 > balanceOf[address(msg.sender)]:
+        revert with 0, 'SafeMath: subtraction overflow'
+    balanceOf[address(msg.sender)] -= arg2
+    if balanceOf[address(arg1)] + arg2 < balanceOf[address(arg1)]:
+        revert with 0, 'SafeMath: addition overflow'
+    balanceOf[address(arg1)] += arg2
+    emit Transfer(arg2, msg.sender, arg1);
+    return 1
+}
+
+function transferFrom(address arg1, address arg2, uint256 arg3) {
+    require calldata.size - 4 >= 96
+    if not arg1:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 'ERC20: transfer from the zero address'
+    if not arg2:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 'ERC20: transfer to the zero address'
+    if arg3 > balanceOf[address(arg1)]:
+        revert with 0, 'SafeMath: subtraction overflow'
+    balanceOf[address(arg1)] -= arg3
+    if balanceOf[address(arg2)] + arg3 < balanceOf[address(arg2)]:
+        revert with 0, 'SafeMath: addition overflow'
+    balanceOf[address(arg2)] += arg3
+    emit Transfer(arg3, arg1, arg2);
+    if arg3 > allowance[address(arg1)][address(msg.sender)]:
+        revert with 0, 'SafeMath: subtraction overflow'
+    if not arg1:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 'ERC20: approve from the zero address'
+    if not msg.sender:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 'ERC20: approve to the zero address'
+    allowance[address(arg1)][address(msg.sender)] -= arg3
+    emit Approval((allowance[address(arg1)][address(msg.sender)] - arg3), arg1, msg.sender);
+    return 1
+}
+
+
+
+}
---
+++ contracts/0x30c11F329542005CcEcE135fBC80B94af7f25c2C.sol
@@ -0,0 +1,207 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+mapping of uint256 balanceOf;
+mapping of uint256 allowance;
+uint256 totalSupply;
+mapping of uint8 stor3;
+array of uint256 name;
+array of uint256 symbol;
+uint8 decimals;
+
+function name() {
+    return name[0 len name.length]
+}
+
+function totalSupply() {
+    return totalSupply
+}
+
+function decimals() {
+    return decimals
+}
+
+function balanceOf(address arg1) {
+    require calldata.size - 4 >= 32
+    return balanceOf[address(arg1)]
+}
+
+function symbol() {
+    return symbol[0 len symbol.length]
+}
+
+function isMinter(address arg1) {
+    require calldata.size - 4 >= 32
+    if not arg1:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 'Roles: account is the zero address'
+    return bool(stor3[address(arg1)])
+}
+
+function allowance(address arg1, address arg2) {
+    require calldata.size - 4 >= 64
+    return allowance[address(arg1)][address(arg2)]
+}
+
+function _fallback() payable {
+    revert
+}
+
+function renounceMinter() {
+    if not msg.sender:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 'Roles: account is the zero address'
+    if not stor3[address(msg.sender)]:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 'Roles: account does not have role'
+    stor3[address(msg.sender)] = 0
+    emit MinterRemoved(msg.sender);
+}
+
+function approve(address arg1, uint256 arg2) {
+    require calldata.size - 4 >= 64
+    if not msg.sender:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 'ERC20: approve from the zero address'
+    if not arg1:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 'ERC20: approve to the zero address'
+    allowance[address(msg.sender)][address(arg1)] = arg2
+    emit Approval(arg2, msg.sender, arg1);
+    return 1
+}
+
+function burn(uint256 arg1) {
+    require calldata.size - 4 >= 32
+    if not msg.sender:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 'ERC20: burn from the zero address'
+    if arg1 > totalSupply:
+        revert with 0, 'SafeMath: subtraction overflow'
+    totalSupply -= arg1
+    if arg1 > balanceOf[address(msg.sender)]:
+        revert with 0, 'SafeMath: subtraction overflow'
+    balanceOf[address(msg.sender)] -= arg1
+    emit Transfer(arg1, msg.sender, 0);
+}
+
+function decreaseAllowance(address arg1, uint256 arg2) {
+    require calldata.size - 4 >= 64
+    if arg2 > allowance[address(msg.sender)][address(arg1)]:
+        revert with 0, 'SafeMath: subtraction overflow'
+    if not msg.sender:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 'ERC20: approve from the zero address'
+    if not arg1:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 'ERC20: approve to the zero address'
+    allowance[address(msg.sender)][address(arg1)] -= arg2
+    emit Approval((allowance[address(msg.sender)][address(arg1)] - arg2), msg.sender, arg1);
+    return 1
+}
+
+function addMinter(address arg1) {
+    require calldata.size - 4 >= 32
+    if not msg.sender:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 'Roles: account is the zero address'
+    if not stor3[address(msg.sender)]:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 'MinterRole: caller does not have the Minter role'
+    if not arg1:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 'Roles: account is the zero address'
+    if stor3[address(arg1)]:
+        revert with 0, 'Roles: account already has role'
+    stor3[address(arg1)] = 1
+    emit MinterAdded(arg1);
+}
+
+function increaseAllowance(address arg1, uint256 arg2) {
+    require calldata.size - 4 >= 64
+    if allowance[address(msg.sender)][address(arg1)] + arg2 < allowance[address(msg.sender)][address(arg1)]:
+        revert with 0, 'SafeMath: addition overflow'
+    if not msg.sender:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 'ERC20: approve from the zero address'
+    if not arg1:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 'ERC20: approve to the zero address'
+    allowance[address(msg.sender)][address(arg1)] += arg2
+    emit Approval((allowance[address(msg.sender)][address(arg1)] + arg2), msg.sender, arg1);
+    return 1
+}
+
+function transfer(address arg1, uint256 arg2) {
+    require calldata.size - 4 >= 64
+    if not msg.sender:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 'ERC20: transfer from the zero address'
+    if not arg1:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 'ERC20: transfer to the zero address'
+    if arg2 > balanceOf[address(msg.sender)]:
+        revert with 0, 'SafeMath: subtraction overflow'
+    balanceOf[address(msg.sender)] -= arg2
+    if balanceOf[address(arg1)] + arg2 < balanceOf[address(arg1)]:
+        revert with 0, 'SafeMath: addition overflow'
+    balanceOf[address(arg1)] += arg2
+    emit Transfer(arg2, msg.sender, arg1);
+    return 1
+}
+
+function mint(address arg1, uint256 arg2) {
+    require calldata.size - 4 >= 64
+    if not msg.sender:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 'Roles: account is the zero address'
+    if not stor3[address(msg.sender)]:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 'MinterRole: caller does not have the Minter role'
+    if not arg1:
+        revert with 0, 'ERC20: mint to the zero address'
+    if totalSupply + arg2 < totalSupply:
+        revert with 0, 'SafeMath: addition overflow'
+    totalSupply += arg2
+    if balanceOf[address(arg1)] + arg2 < balanceOf[address(arg1)]:
+        revert with 0, 'SafeMath: addition overflow'
+    balanceOf[address(arg1)] += arg2
+    emit Transfer(arg2, 0, arg1);
+    return 1
+}
+
+function transferMinterRole(address arg1) {
+    require calldata.size - 4 >= 32
+    if not msg.sender:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 'Roles: account is the zero address'
+    if not stor3[address(msg.sender)]:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 'MinterRole: caller does not have the Minter role'
+    if not arg1:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 'Roles: account is the zero address'
+    if stor3[address(arg1)]:
+        revert with 0, 'Roles: account already has role'
+    stor3[address(arg1)] = 1
+    emit MinterAdded(arg1);
+    if not msg.sender:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 'Roles: account is the zero address'
+    if not stor3[address(msg.sender)]:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 'Roles: account does not have role'
+    stor3[address(msg.sender)] = 0
+    emit MinterRemoved(msg.sender);
+}
+
+function transferFrom(address arg1, address arg2, uint256 arg3) {
+    require calldata.size - 4 >= 96
+    if not arg1:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 'ERC20: transfer from the zero address'
+    if not arg2:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 'ERC20: transfer to the zero address'
+    if arg3 > balanceOf[address(arg1)]:
+        revert with 0, 'SafeMath: subtraction overflow'
+    balanceOf[address(arg1)] -= arg3
+    if balanceOf[address(arg2)] + arg3 < balanceOf[address(arg2)]:
+        revert with 0, 'SafeMath: addition overflow'
+    balanceOf[address(arg2)] += arg3
+    emit Transfer(arg3, arg1, arg2);
+    if arg3 > allowance[address(arg1)][address(msg.sender)]:
+        revert with 0, 'SafeMath: subtraction overflow'
+    if not arg1:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 'ERC20: approve from the zero address'
+    if not msg.sender:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 'ERC20: approve to the zero address'
+    allowance[address(arg1)][address(msg.sender)] -= arg3
+    emit Approval((allowance[address(arg1)][address(msg.sender)] - arg3), arg1, msg.sender);
+    return 1
+}
+
+
+
+}
