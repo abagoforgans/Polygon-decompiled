5f4fb4b64977a576f3c1c0c4b7cbc292da066481
abagoforgans <abagoforgans@users.noreply.github.com>
2022-06-22 07:18:47 +0000

    autocommit

---
+++ contracts/0x3975f669011683155817EFf41a0E17A72ddCd545.sol
@@ -0,0 +1,330 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+address owner;
+mapping of uint256 balanceOf;
+mapping of uint256 allowance;
+uint256 totalSupply;
+array of uint256 name;
+array of uint256 symbol;
+uint8 decimals;
+
+function name() payable {
+    return name[0 len name.length]
+}
+
+function totalSupply() payable {
+    return totalSupply
+}
+
+function decimals() payable {
+    return decimals
+}
+
+function balanceOf(address arg1) payable {
+    require calldata.size - 4 >= 32
+    return balanceOf[address(arg1)]
+}
+
+function owner() payable {
+    return owner
+}
+
+function symbol() payable {
+    return symbol[0 len symbol.length]
+}
+
+function allowance(address arg1, address arg2) payable {
+    require calldata.size - 4 >= 64
+    return allowance[address(arg1)][address(arg2)]
+}
+
+function _fallback() payable {
+    revert
+}
+
+function renounceOwnership() payable {
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    emit OwnershipTransferred(owner, 0);
+    owner = 0
+}
+
+function transferOwnership(address arg1) payable {
+    require calldata.size - 4 >= 32
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    if not arg1:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    38,
+                    0x654f776e61626c653a206e6577206f776e657220697320746865207a65726f20616464726573,
+                    mem[202 len 26]
+    emit OwnershipTransferred(owner, arg1);
+    owner = arg1
+}
+
+function mintToken(address arg1, uint256 arg2) payable {
+    require calldata.size - 4 >= 64
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    if not arg1:
+        revert with 0, 'ERC20: mint to the zero address'
+    if totalSupply + arg2 < totalSupply:
+        revert with 0, 'SafeMath: addition overflow'
+    totalSupply += arg2
+    if balanceOf[address(arg1)] + arg2 < balanceOf[address(arg1)]:
+        revert with 0, 'SafeMath: addition overflow'
+    balanceOf[address(arg1)] += arg2
+    emit Transfer(arg2, 0, arg1);
+}
+
+function approve(address arg1, uint256 arg2) payable {
+    require calldata.size - 4 >= 64
+    if not msg.sender:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    36,
+                    0x7345524332303a20617070726f76652066726f6d20746865207a65726f20616464726573,
+                    mem[200 len 28]
+    if not arg1:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    34,
+                    0x7345524332303a20617070726f766520746f20746865207a65726f20616464726573,
+                    mem[198 len 30]
+    allowance[address(msg.sender)][address(arg1)] = arg2
+    emit Approval(arg2, msg.sender, arg1);
+    return 1
+}
+
+function burnToken(address arg1, uint256 arg2) payable {
+    require calldata.size - 4 >= 64
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    if not arg1:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    33,
+                    0x6545524332303a206275726e2066726f6d20746865207a65726f20616464726573,
+                    mem[197 len 31]
+    if arg2 > balanceOf[address(arg1)]:
+        revert with 0, 32, 34, 0x7345524332303a206275726e20616d6f756e7420657863656564732062616c616e63, mem[162 len 30], mem[222 len 2]
+    balanceOf[address(arg1)] -= arg2
+    if arg2 > totalSupply:
+        revert with 0, 'SafeMath: subtraction overflow'
+    totalSupply -= arg2
+    emit Transfer(arg2, arg1, 0);
+}
+
+function decreaseAllowance(address arg1, uint256 arg2) payable {
+    require calldata.size - 4 >= 64
+    if arg2 > allowance[address(msg.sender)][address(arg1)]:
+        revert with 0, 
+                    32,
+                    37,
+                    0x7345524332303a2064656372656173656420616c6c6f77616e63652062656c6f77207a6572,
+                    mem[165 len 27],
+                    mem[219 len 5]
+    if not msg.sender:
+        revert with 0, 32, 36, 0x7345524332303a20617070726f76652066726f6d20746865207a65726f20616464726573, mem[296 len 28]
+    if not arg1:
+        revert with 0, 32, 34, 0x7345524332303a20617070726f766520746f20746865207a65726f20616464726573, mem[294 len 30]
+    allowance[address(msg.sender)][address(arg1)] -= arg2
+    emit Approval((allowance[address(msg.sender)][address(arg1)] - arg2), msg.sender, arg1);
+    return 1
+}
+
+function increaseAllowance(address arg1, uint256 arg2) payable {
+    require calldata.size - 4 >= 64
+    if allowance[address(msg.sender)][address(arg1)] + arg2 < allowance[address(msg.sender)][address(arg1)]:
+        revert with 0, 'SafeMath: addition overflow'
+    if not msg.sender:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    36,
+                    0x7345524332303a20617070726f76652066726f6d20746865207a65726f20616464726573,
+                    mem[200 len 28]
+    if not arg1:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    34,
+                    0x7345524332303a20617070726f766520746f20746865207a65726f20616464726573,
+                    mem[198 len 30]
+    allowance[address(msg.sender)][address(arg1)] += arg2
+    emit Approval((allowance[address(msg.sender)][address(arg1)] + arg2), msg.sender, arg1);
+    return 1
+}
+
+function transfer(address arg1, uint256 arg2) payable {
+    require calldata.size - 4 >= 64
+    if not msg.sender:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    37,
+                    0x7345524332303a207472616e736665722066726f6d20746865207a65726f20616464726573,
+                    mem[201 len 27]
+    if not arg1:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    35,
+                    0xfe45524332303a207472616e7366657220746f20746865207a65726f20616464726573,
+                    mem[199 len 29]
+    if arg2 > balanceOf[address(msg.sender)]:
+        revert with 0, 
+                    32,
+                    38,
+                    0x7345524332303a207472616e7366657220616d6f756e7420657863656564732062616c616e63,
+                    mem[166 len 26],
+                    mem[218 len 6]
+    balanceOf[address(msg.sender)] -= arg2
+    if balanceOf[address(arg1)] + arg2 < balanceOf[address(arg1)]:
+        revert with 0, 'SafeMath: addition overflow'
+    balanceOf[address(arg1)] += arg2
+    emit Transfer(arg2, msg.sender, arg1);
+    return 1
+}
+
+function sub_c471469f(?) payable {
+    require calldata.size - 4 >= 64
+    require arg1 <= 4294967296
+    require arg1 + 36 <= calldata.size
+    require arg1.length <= 4294967296 and arg1 + (32 * arg1.length) + 36 <= calldata.size
+    mem[128 len 32 * arg1.length] = call.data[arg1 + 36 len 32 * arg1.length]
+    mem[(32 * arg1.length) + 128] = 0
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    idx = 0
+    while idx < arg1.length:
+        require idx < arg1.length
+        _19 = mem[(32 * idx) + 128]
+        if not mem[(32 * idx) + 140 len 20]:
+            revert with 0, 'ERC20: mint to the zero address'
+        if totalSupply + arg2 < totalSupply:
+            revert with 0, 'SafeMath: addition overflow'
+        totalSupply += arg2
+        if balanceOf[mem[(32 * idx) + 140 len 20]] + arg2 < balanceOf[mem[(32 * idx) + 140 len 20]]:
+            revert with 0, 'SafeMath: addition overflow'
+        mem[0] = mem[(32 * idx) + 140 len 20]
+        mem[32] = 1
+        balanceOf[address(mem[(32 * idx) + 128])] = balanceOf[mem[(32 * idx) + 140 len 20]] + arg2
+        mem[(32 * arg1.length) + 128] = arg2
+        emit Transfer(arg2, 0, address(_19));
+        idx = idx + 1
+        continue 
+}
+
+function transferFrom(address arg1, address arg2, uint256 arg3) payable {
+    require calldata.size - 4 >= 96
+    if not arg1:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    37,
+                    0x7345524332303a207472616e736665722066726f6d20746865207a65726f20616464726573,
+                    mem[201 len 27]
+    if not arg2:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    35,
+                    0xfe45524332303a207472616e7366657220746f20746865207a65726f20616464726573,
+                    mem[199 len 29]
+    if arg3 > balanceOf[address(arg1)]:
+        revert with 0, 
+                    32,
+                    38,
+                    0x7345524332303a207472616e7366657220616d6f756e7420657863656564732062616c616e63,
+                    mem[166 len 26],
+                    mem[218 len 6]
+    balanceOf[address(arg1)] -= arg3
+    if balanceOf[address(arg2)] + arg3 < balanceOf[address(arg2)]:
+        revert with 0, 'SafeMath: addition overflow'
+    balanceOf[address(arg2)] += arg3
+    emit Transfer(arg3, arg1, arg2);
+    if arg3 > allowance[address(arg1)][address(msg.sender)]:
+        revert with 0, 
+                    32,
+                    40,
+                    0x6545524332303a207472616e7366657220616d6f756e74206578636565647320616c6c6f77616e63,
+                    mem[264 len 24],
+                    mem[312 len 8]
+    if not arg1:
+        revert with 0, 32, 36, 0x7345524332303a20617070726f76652066726f6d20746865207a65726f20616464726573, mem[392 len 28]
+    if not msg.sender:
+        revert with 0, 32, 34, 0x7345524332303a20617070726f766520746f20746865207a65726f20616464726573, mem[390 len 30]
+    allowance[address(arg1)][address(msg.sender)] -= arg3
+    emit Approval((allowance[address(arg1)][address(msg.sender)] - arg3), arg1, msg.sender);
+    return 1
+}
+
+function sub_fad79c16(?) payable {
+    require calldata.size - 4 >= 64
+    require arg1 <= 4294967296
+    require arg1 + 36 <= calldata.size
+    require arg1.length <= 4294967296 and arg1 + (32 * arg1.length) + 36 <= calldata.size
+    mem[64] = (32 * arg1.length) + 128
+    mem[96] = arg1.length
+    mem[128 len 32 * arg1.length] = call.data[arg1 + 36 len 32 * arg1.length]
+    mem[(32 * arg1.length) + 128] = 0
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    idx = 0
+    while idx < arg1.length:
+        require idx < mem[96]
+        _31 = mem[(32 * idx) + 128]
+        if not mem[(32 * idx) + 140 len 20]:
+            revert with 0, 32, 33, 0x6545524332303a206275726e2066726f6d20746865207a65726f20616464726573, mem[mem[64] + 101 len 31]
+        _34 = mem[64]
+        mem[64] = mem[64] + 96
+        mem[_34] = 34
+        mem[_34 + 32 len 34] = 0x7345524332303a206275726e20616d6f756e7420657863656564732062616c616e63
+        mem[32] = 1
+        if arg2 > balanceOf[address(_31)]:
+            _36 = mem[64]
+            mem[mem[64]] = 0x8c379a000000000000000000000000000000000000000000000000000000000
+            mem[mem[64] + 4] = 32
+            mem[mem[64] + 36] = 34
+            idx = 0
+            while idx < 34:
+                mem[_36 + idx + 68] = mem[_34 + idx + 32]
+                idx = idx + 32
+                continue 
+            mem[_36 + 100] = mem[_36 + 130 len 2]
+            revert with memory
+              from mem[64]
+               len _36 + -mem[64] + 132
+        mem[0] = address(_31)
+        mem[32] = 1
+        balanceOf[address(_31)] -= arg2
+        _40 = mem[64]
+        mem[64] = mem[64] + 64
+        mem[_40] = 30
+        mem[_40 + 32] = 'SafeMath: subtraction overflow'
+        if arg2 <= totalSupply:
+            totalSupply -= arg2
+            mem[mem[64]] = arg2
+            emit Transfer(arg2, address(_31), 0);
+            idx = idx + 1
+            continue 
+        _44 = mem[64]
+        mem[mem[64]] = 0x8c379a000000000000000000000000000000000000000000000000000000000
+        mem[mem[64] + 4] = 32
+        mem[mem[64] + 36] = 30
+        s = 0
+        while s < 30:
+            mem[_44 + s + 68] = mem[_40 + s + 32]
+            s = s + 32
+            continue 
+        mem[_44 + 68] = mem[_44 + 70 len 30]
+        revert with memory
+          from mem[64]
+           len _44 + -mem[64] + 100
+}
+
+
+
+}
---
+++ contracts/0x5d43c87EfE9409E0fe32a5729DfF47BB15ec0243.sol
@@ -0,0 +1,230 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+address owner;
+mapping of uint256 balanceOf;
+mapping of uint256 allowance;
+uint256 totalSupply;
+array of uint256 name;
+array of uint256 symbol;
+uint8 decimals;
+
+function name() payable {
+    return name[0 len name.length]
+}
+
+function totalSupply() payable {
+    return totalSupply
+}
+
+function decimals() payable {
+    return decimals
+}
+
+function balanceOf(address arg1) payable {
+    require calldata.size - 4 >= 32
+    return balanceOf[address(arg1)]
+}
+
+function owner() payable {
+    return owner
+}
+
+function symbol() payable {
+    return symbol[0 len symbol.length]
+}
+
+function allowance(address arg1, address arg2) payable {
+    require calldata.size - 4 >= 64
+    return allowance[address(arg1)][address(arg2)]
+}
+
+function _fallback() payable {
+    revert
+}
+
+function renounceOwnership() payable {
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    emit OwnershipTransferred(owner, 0);
+    owner = 0
+}
+
+function transferOwnership(address arg1) payable {
+    require calldata.size - 4 >= 32
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    if not arg1:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    38,
+                    0x734f776e61626c653a206e6577206f776e657220697320746865207a65726f20616464726573,
+                    mem[202 len 26]
+    emit OwnershipTransferred(owner, arg1);
+    owner = arg1
+}
+
+function approve(address arg1, uint256 arg2) payable {
+    require calldata.size - 4 >= 64
+    if not msg.sender:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    36,
+                    0x7345524332303a20617070726f76652066726f6d20746865207a65726f20616464726573,
+                    mem[200 len 28]
+    if not arg1:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    34,
+                    0x7345524332303a20617070726f766520746f20746865207a65726f20616464726573,
+                    mem[198 len 30]
+    allowance[address(msg.sender)][address(arg1)] = arg2
+    emit Approval(arg2, msg.sender, arg1);
+    return 1
+}
+
+function decreaseAllowance(address arg1, uint256 arg2) payable {
+    require calldata.size - 4 >= 64
+    if arg2 > allowance[address(msg.sender)][address(arg1)]:
+        revert with 0, 
+                    32,
+                    37,
+                    0x7345524332303a2064656372656173656420616c6c6f77616e63652062656c6f77207a6572,
+                    mem[165 len 27],
+                    mem[219 len 5]
+    if not msg.sender:
+        revert with 0, 32, 36, 0x7345524332303a20617070726f76652066726f6d20746865207a65726f20616464726573, mem[296 len 28]
+    if not arg1:
+        revert with 0, 32, 34, 0x7345524332303a20617070726f766520746f20746865207a65726f20616464726573, mem[294 len 30]
+    allowance[address(msg.sender)][address(arg1)] -= arg2
+    emit Approval((allowance[address(msg.sender)][address(arg1)] - arg2), msg.sender, arg1);
+    return 1
+}
+
+function increaseAllowance(address arg1, uint256 arg2) payable {
+    require calldata.size - 4 >= 64
+    if allowance[address(msg.sender)][address(arg1)] + arg2 < allowance[address(msg.sender)][address(arg1)]:
+        revert with 0, 'SafeMath: addition overflow'
+    if not msg.sender:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    36,
+                    0x7345524332303a20617070726f76652066726f6d20746865207a65726f20616464726573,
+                    mem[200 len 28]
+    if not arg1:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    34,
+                    0x7345524332303a20617070726f766520746f20746865207a65726f20616464726573,
+                    mem[198 len 30]
+    allowance[address(msg.sender)][address(arg1)] += arg2
+    emit Approval((allowance[address(msg.sender)][address(arg1)] + arg2), msg.sender, arg1);
+    return 1
+}
+
+function sub_f9448846(?) payable {
+    require calldata.size - 4 >= 32
+    require arg1 <= 4294967296
+    require arg1 + 36 <= calldata.size
+    require arg1.length <= 4294967296 and arg1 + (32 * arg1.length) + 36 <= calldata.size
+    mem[128 len 32 * arg1.length] = call.data[arg1 + 36 len 32 * arg1.length]
+    mem[(32 * arg1.length) + 128] = 0
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    require arg1.length
+    idx = 0
+    while idx < arg1.length:
+        require idx < arg1.length
+        mem[(32 * arg1.length) + 132] = mem[(32 * idx) + 140 len 20]
+        mem[(32 * arg1.length) + 164] = 1000 * 10^18 / arg1.length
+        require ext_code.size(this.address)
+        call this.address.0xa9059cbb with:
+             gas gas_remaining wei
+            args mem[(32 * arg1.length) + 132], 1000 * 10^18 / arg1.length
+        mem[(32 * arg1.length) + 128] = ext_call.return_data[0]
+        if not ext_call.success:
+            revert with ext_call.return_data[0 len return_data.size]
+        require return_data.size >= 32
+        idx = idx + 1
+        continue 
+}
+
+function transfer(address arg1, uint256 arg2) payable {
+    require calldata.size - 4 >= 64
+    if not msg.sender:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    37,
+                    0x6545524332303a207472616e736665722066726f6d20746865207a65726f20616464726573,
+                    mem[201 len 27]
+    if not arg1:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    35,
+                    0xfe45524332303a207472616e7366657220746f20746865207a65726f20616464726573,
+                    mem[199 len 29]
+    if arg2 > balanceOf[address(msg.sender)]:
+        revert with 0, 
+                    32,
+                    38,
+                    0x7345524332303a207472616e7366657220616d6f756e7420657863656564732062616c616e63,
+                    mem[166 len 26],
+                    mem[218 len 6]
+    balanceOf[address(msg.sender)] -= arg2
+    if balanceOf[address(arg1)] + arg2 < balanceOf[address(arg1)]:
+        revert with 0, 'SafeMath: addition overflow'
+    balanceOf[address(arg1)] += arg2
+    emit Transfer(arg2, msg.sender, arg1);
+    return 1
+}
+
+function transferFrom(address arg1, address arg2, uint256 arg3) payable {
+    require calldata.size - 4 >= 96
+    if not arg1:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    37,
+                    0x6545524332303a207472616e736665722066726f6d20746865207a65726f20616464726573,
+                    mem[201 len 27]
+    if not arg2:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    35,
+                    0xfe45524332303a207472616e7366657220746f20746865207a65726f20616464726573,
+                    mem[199 len 29]
+    if arg3 > balanceOf[address(arg1)]:
+        revert with 0, 
+                    32,
+                    38,
+                    0x7345524332303a207472616e7366657220616d6f756e7420657863656564732062616c616e63,
+                    mem[166 len 26],
+                    mem[218 len 6]
+    balanceOf[address(arg1)] -= arg3
+    if balanceOf[address(arg2)] + arg3 < balanceOf[address(arg2)]:
+        revert with 0, 'SafeMath: addition overflow'
+    balanceOf[address(arg2)] += arg3
+    emit Transfer(arg3, arg1, arg2);
+    if arg3 > allowance[address(arg1)][address(msg.sender)]:
+        revert with 0, 
+                    32,
+                    40,
+                    0x6545524332303a207472616e7366657220616d6f756e74206578636565647320616c6c6f77616e63,
+                    mem[264 len 24],
+                    mem[312 len 8]
+    if not arg1:
+        revert with 0, 32, 36, 0x7345524332303a20617070726f76652066726f6d20746865207a65726f20616464726573, mem[392 len 28]
+    if not msg.sender:
+        revert with 0, 32, 34, 0x7345524332303a20617070726f766520746f20746865207a65726f20616464726573, mem[390 len 30]
+    allowance[address(arg1)][address(msg.sender)] -= arg3
+    emit Approval((allowance[address(arg1)][address(msg.sender)] - arg3), arg1, msg.sender);
+    return 1
+}
+
+
+
+}
