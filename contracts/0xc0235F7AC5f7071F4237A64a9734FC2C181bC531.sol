3714d8bedd526c1d474a69ac08d3eea52ef4ec62
abagoforgans <abagoforgans@users.noreply.github.com>
2022-06-24 16:55:05 +0000

    autocommit

---
+++ contracts/0xc0235F7AC5f7071F4237A64a9734FC2C181bC531.sol
@@ -0,0 +1,65 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+function _fallback() payable {
+    revert
+}
+
+function sub_f522696d(?) payable {
+    require calldata.size - 4 >= 96
+    require cd[36] <= 4294967296
+    require cd[36] + 36 <= calldata.size
+    require ('cd', 36).length <= 4294967296 and cd[36] + (32 * ('cd', 36).length) + 36 <= calldata.size
+    idx = 0
+    while idx < ('cd', 36).length:
+        mem[100] = msg.sender
+        mem[132] = address(cd[((32 * idx) + cd[36] + 36)])
+        mem[164] = cd[68]
+        require ext_code.size(address(cd[4]))
+        call address(cd[4]).transferFrom(address arg1, address arg2, uint256 arg3) with:
+             gas gas_remaining wei
+            args msg.sender, address(cd[((32 * idx) + cd[36] + 36)]), cd[68]
+        mem[96] = ext_call.return_data[0]
+        if not ext_call.success:
+            revert with ext_call.return_data[0 len return_data.size]
+        require return_data.size >= 32
+        idx = idx + 1
+        continue 
+}
+
+function sub_40aa070c(?) payable {
+    require calldata.size - 4 >= 96
+    require cd[36] <= 4294967296
+    require cd[36] + 36 <= calldata.size
+    require ('cd', 36).length <= 4294967296 and cd[36] + (32 * ('cd', 36).length) + 36 <= calldata.size
+    require cd[68] <= 4294967296
+    require cd[68] + 36 <= calldata.size
+    require ('cd', 68).length <= 4294967296 and cd[68] + (32 * ('cd', 68).length) + 36 <= calldata.size
+    if ('cd', 36).length != ('cd', 68).length:
+        revert with 0, 'Arrays lengths not equal'
+    idx = 0
+    while idx < ('cd', 36).length:
+        require idx < ('cd', 68).length
+        mem[100] = msg.sender
+        mem[132] = address(cd[((32 * idx) + cd[36] + 36)])
+        mem[164] = cd[((32 * idx) + cd[68] + 36)]
+        require ext_code.size(address(cd[4]))
+        call address(cd[4]).transferFrom(address arg1, address arg2, uint256 arg3) with:
+             gas gas_remaining wei
+            args msg.sender, address(cd[((32 * idx) + cd[36] + 36)]), cd[((32 * idx) + cd[68] + 36)]
+        mem[96] = ext_call.return_data[0]
+        if not ext_call.success:
+            revert with ext_call.return_data[0 len return_data.size]
+        require return_data.size >= 32
+        idx = idx + 1
+        continue 
+}
+
+
+
+}
