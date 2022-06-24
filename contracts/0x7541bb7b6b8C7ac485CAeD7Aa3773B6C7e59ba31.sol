d54b3b1f39addaca36570f335888c80f8e32d63b
abagoforgans <abagoforgans@users.noreply.github.com>
2022-06-24 13:54:44 +0000

    autocommit

---
+++ contracts/0x7541bb7b6b8C7ac485CAeD7Aa3773B6C7e59ba31.sol
@@ -0,0 +1,23 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+const works = 'it works'
+
+
+function _fallback() payable {
+    revert
+}
+
+function sub_24dcf6b9(?) payable {
+    require calldata.size - 4 >= 32
+    return arg1
+}
+
+
+
+}
