ecf29ddd994e42e0b10e5b324fd548cc7919b22d
abagoforgans <abagoforgans@users.noreply.github.com>
2022-06-26 17:22:17 +0000

    autocommit

---
+++ contracts/0xb0897686c545045aFc77CF20eC7A532E3120E0F1.sol
@@ -0,0 +1,111 @@
+contract main {
+
+
+// =======================  Init code  ======================
+
+
+mapping of uint256 stor1;
+
+function _fallback() {
+    stor1[address(msg.sender)] = 1000000000 * 10^18
+    return code.data[72 len 3153]
+}
+
+
+
+// =====================  Runtime code  =====================
+
+
+const name = 'ChainLink Token'
+
+const totalSupply = 1000000000 * 10^18
+
+const decimals = 18
+
+const symbol = 'LINK'
+
+
+mapping of uint256 balanceOf;
+mapping of uint256 allowance;
+
+function balanceOf(address arg1) {
+    return balanceOf[address(arg1)]
+}
+
+function allowance(address arg1, address arg2) {
+    return allowance[address(arg1)][address(arg2)]
+}
+
+function _fallback() payable {
+    revert
+}
+
+function approve(address arg1, uint256 arg2) {
+    require arg1
+    require arg1 != this.address
+    allowance[address(msg.sender)][address(arg1)] = arg2
+    emit Approval(arg2, msg.sender, arg1);
+    return 1
+}
+
+function increaseApproval(address arg1, uint256 arg2) {
+    require arg2 + allowance[address(msg.sender)][address(arg1)] >= allowance[address(msg.sender)][address(arg1)]
+    allowance[address(msg.sender)][address(arg1)] += arg2
+    emit Approval((arg2 + allowance[address(msg.sender)][address(arg1)]), msg.sender, arg1);
+    return 1
+}
+
+function decreaseApproval(address arg1, uint256 arg2) {
+    if arg2 <= allowance[address(msg.sender)][address(arg1)]:
+        allowance[address(msg.sender)][address(arg1)] -= arg2
+    else:
+        allowance[address(msg.sender)][address(arg1)] = 0
+    emit Approval(allowance[address(msg.sender)][address(arg1)], msg.sender, arg1);
+    return 1
+}
+
+function transfer(address arg1, uint256 arg2) {
+    require arg1
+    require arg1 != this.address
+    require arg2 <= balanceOf[address(msg.sender)]
+    balanceOf[address(msg.sender)] -= arg2
+    require arg2 + balanceOf[arg1] >= balanceOf[arg1]
+    balanceOf[address(arg1)] = arg2 + balanceOf[arg1]
+    emit Transfer(arg2, msg.sender, arg1);
+    return 1
+}
+
+function transferFrom(address arg1, address arg2, uint256 arg3) {
+    require arg2
+    require arg2 != this.address
+    require arg3 <= balanceOf[address(arg1)]
+    balanceOf[address(arg1)] -= arg3
+    require arg3 + balanceOf[arg2] >= balanceOf[arg2]
+    balanceOf[address(arg2)] = arg3 + balanceOf[arg2]
+    require arg3 <= allowance[address(arg1)][address(msg.sender)]
+    allowance[address(arg1)][address(msg.sender)] -= arg3
+    emit Transfer(arg3, arg1, arg2);
+    return 1
+}
+
+function transferAndCall(address arg1, uint256 arg2, bytes arg3) {
+    require arg1
+    require arg1 != this.address
+    require arg2 <= balanceOf[address(msg.sender)]
+    balanceOf[address(msg.sender)] -= arg2
+    require arg2 + balanceOf[arg1] >= balanceOf[arg1]
+    balanceOf[address(arg1)] = arg2 + balanceOf[arg1]
+    emit Transfer(arg2, msg.sender, arg1);
+    emit Transfer(arg2, Array(len=arg3.length, data=arg3[all]), msg.sender, arg1);
+    if ext_code.size(arg1) > 0:
+        require ext_code.size(arg1)
+        call arg1.onTokenTransfer(address arg1, uint256 arg2, bytes arg3) with:
+             gas gas_remaining - 710 wei
+            args msg.sender, arg2, Array(len=arg3.length, data=arg3[all])
+        require ext_call.success
+    return 1
+}
+
+
+
+}
