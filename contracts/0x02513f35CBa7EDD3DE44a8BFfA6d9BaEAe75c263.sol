74082b430d0851ef5dea3c77135d18e7822c3b07
abagoforgans <abagoforgans@users.noreply.github.com>
2022-06-30 01:16:14 +0000

    autocommit

---
+++ contracts/0x02513f35CBa7EDD3DE44a8BFfA6d9BaEAe75c263.sol
@@ -0,0 +1,27 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+array of struct stor0;
+
+function _fallback() payable {
+    revert
+}
+
+function sub_611dc058(?) payable {
+    require calldata.size - 4 >= 64
+    require arg1 == arg1
+    require arg2 == arg2
+    stor0[arg2].field_0++
+    stor0[arg2][stor0[arg2].field_0].field_0 = arg1
+    stor0[arg2][stor0[arg2].field_0].field_256 = block.timestamp
+    emit 0x89513cdd: arg2, arg1, block.timestamp
+}
+
+
+
+}
---
+++ contracts/0x3c563F06ED1Bf9a8Ea1c0BDf7d481F6241C75F23.sol
@@ -0,0 +1,31 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+array of struct stor0;
+
+function _fallback() payable {
+    revert
+}
+
+function sub_4a698cce(?) payable {
+    require calldata.size - 4 >= 64
+    require arg1 <= test266151307()
+    require arg1 + 35 < calldata.size
+    require arg1.length <= test266151307()
+    require ceil32(arg1.length) + 128 >= 96 and ceil32(arg1.length) + 128 <= test266151307()
+    require arg1 + arg1.length + 36 <= calldata.size
+    require arg2 == arg2
+    stor0[arg2].field_0++
+    stor0[arg2][stor0[arg2].field_0][].field_0 = Array(len=arg1.length, data=arg1[all])
+    stor0[arg2][stor0[arg2].field_0].field_256 = block.timestamp
+    emit 0x64127525: arg2, Array(len=arg1.length, data=arg1[all]), block.timestamp
+}
+
+
+
+}
