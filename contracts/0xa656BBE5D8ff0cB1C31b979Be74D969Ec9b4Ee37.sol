9e092708745d16fac36f326d9ffe0cfbd116c62d
abagoforgans <abagoforgans@users.noreply.github.com>
2022-07-05 15:16:13 +0000

    autocommit

---
+++ contracts/0xa656BBE5D8ff0cB1C31b979Be74D969Ec9b4Ee37.sol
@@ -0,0 +1,64 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+address owner;
+mapping of uint8 stor1;
+
+function owner() payable {
+    return owner
+}
+
+function whitelist(address arg1) payable {
+    require calldata.size - 4 >= 32
+    return bool(stor1[arg1])
+}
+
+function _fallback() payable {
+    revert
+}
+
+function renounceOwnership() payable {
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    emit OwnershipTransferred(owner, 0);
+    owner = 0
+}
+
+function addToWhitelist(address arg1) payable {
+    require calldata.size - 4 >= 32
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    stor1[address(arg1)] = 1
+    emit AddedToWhitelist(arg1);
+}
+
+function removeFromWhitelist(address arg1) payable {
+    require calldata.size - 4 >= 32
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    stor1[address(arg1)] = 0
+    emit RemovedFromWhitelist(arg1);
+}
+
+function transferOwnership(address arg1) payable {
+    require calldata.size - 4 >= 32
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    if not arg1:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    38,
+                    0xfe4f776e61626c653a206e6577206f776e657220697320746865207a65726f20616464726573,
+                    mem[202 len 26]
+    emit OwnershipTransferred(owner, arg1);
+    owner = arg1
+}
+
+
+
+}
