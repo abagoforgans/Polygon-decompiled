b4e096f375e8b52cd0b8b454764c1eae3f0f8e61
abagoforgans <abagoforgans@users.noreply.github.com>
2022-07-05 07:52:06 +0000

    autocommit

---
+++ contracts/0x6793d8b7A55390EeEed3f5B5Dca636D2208335a3.sol
@@ -0,0 +1,177 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+mapping of uint256 balanceOf;
+mapping of uint256 allowance;
+uint256 totalSupply;
+array of uint256 name;
+array of uint256 symbol;
+uint8 decimals;
+
+function name() payable {
+    return name[0 len name.length]
+}
+
+function totalSupply() payable {
+    return totalSupply
+}
+
+function decimals() payable {
+    return decimals
+}
+
+function balanceOf(address arg1) payable {
+    require calldata.size - 4 >= 32
+    return balanceOf[address(arg1)]
+}
+
+function symbol() payable {
+    return symbol[0 len symbol.length]
+}
+
+function allowance(address arg1, address arg2) payable {
+    require calldata.size - 4 >= 64
+    return allowance[address(arg1)][address(arg2)]
+}
+
+function _fallback() payable {
+    revert
+}
+
+function approve(address arg1, uint256 arg2) payable {
+    require calldata.size - 4 >= 64
+    if not msg.sender:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    36,
+                    0x7345524332303a20617070726f76652066726f6d20746865207a65726f20616464726573,
+                    mem[200 len 28]
+    if not arg1:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    34,
+                    0x7345524332303a20617070726f766520746f20746865207a65726f20616464726573,
+                    mem[198 len 30]
+    allowance[address(msg.sender)][address(arg1)] = arg2
+    emit Approval(arg2, msg.sender, arg1);
+    return 1
+}
+
+function decreaseAllowance(address arg1, uint256 arg2) payable {
+    require calldata.size - 4 >= 64
+    if arg2 > allowance[address(msg.sender)][address(arg1)]:
+        revert with 0, 
+                    32,
+                    37,
+                    0x7345524332303a2064656372656173656420616c6c6f77616e63652062656c6f77207a6572,
+                    mem[165 len 27],
+                    mem[219 len 5]
+    if not msg.sender:
+        revert with 0, 32, 36, 0x7345524332303a20617070726f76652066726f6d20746865207a65726f20616464726573, mem[296 len 28]
+    if not arg1:
+        revert with 0, 32, 34, 0x7345524332303a20617070726f766520746f20746865207a65726f20616464726573, mem[294 len 30]
+    allowance[address(msg.sender)][address(arg1)] -= arg2
+    emit Approval((allowance[address(msg.sender)][address(arg1)] - arg2), msg.sender, arg1);
+    return 1
+}
+
+function increaseAllowance(address arg1, uint256 arg2) payable {
+    require calldata.size - 4 >= 64
+    if allowance[address(msg.sender)][address(arg1)] + arg2 < allowance[address(msg.sender)][address(arg1)]:
+        revert with 0, 'SafeMath: addition overflow'
+    if not msg.sender:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    36,
+                    0x7345524332303a20617070726f76652066726f6d20746865207a65726f20616464726573,
+                    mem[200 len 28]
+    if not arg1:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    34,
+                    0x7345524332303a20617070726f766520746f20746865207a65726f20616464726573,
+                    mem[198 len 30]
+    allowance[address(msg.sender)][address(arg1)] += arg2
+    emit Approval((allowance[address(msg.sender)][address(arg1)] + arg2), msg.sender, arg1);
+    return 1
+}
+
+function transfer(address arg1, uint256 arg2) payable {
+    require calldata.size - 4 >= 64
+    if not msg.sender:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    37,
+                    0x6545524332303a207472616e736665722066726f6d20746865207a65726f20616464726573,
+                    mem[201 len 27]
+    if not arg1:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    35,
+                    0xfe45524332303a207472616e7366657220746f20746865207a65726f20616464726573,
+                    mem[199 len 29]
+    if arg2 > balanceOf[address(msg.sender)]:
+        revert with 0, 
+                    32,
+                    38,
+                    0x7345524332303a207472616e7366657220616d6f756e7420657863656564732062616c616e63,
+                    mem[166 len 26],
+                    mem[218 len 6]
+    balanceOf[address(msg.sender)] -= arg2
+    if balanceOf[address(arg1)] + arg2 < balanceOf[address(arg1)]:
+        revert with 0, 'SafeMath: addition overflow'
+    balanceOf[address(arg1)] += arg2
+    emit Transfer(arg2, msg.sender, arg1);
+    return 1
+}
+
+function transferFrom(address arg1, address arg2, uint256 arg3) payable {
+    require calldata.size - 4 >= 96
+    if not arg1:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    37,
+                    0x6545524332303a207472616e736665722066726f6d20746865207a65726f20616464726573,
+                    mem[201 len 27]
+    if not arg2:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    35,
+                    0xfe45524332303a207472616e7366657220746f20746865207a65726f20616464726573,
+                    mem[199 len 29]
+    if arg3 > balanceOf[address(arg1)]:
+        revert with 0, 
+                    32,
+                    38,
+                    0x7345524332303a207472616e7366657220616d6f756e7420657863656564732062616c616e63,
+                    mem[166 len 26],
+                    mem[218 len 6]
+    balanceOf[address(arg1)] -= arg3
+    if balanceOf[address(arg2)] + arg3 < balanceOf[address(arg2)]:
+        revert with 0, 'SafeMath: addition overflow'
+    balanceOf[address(arg2)] += arg3
+    emit Transfer(arg3, arg1, arg2);
+    if arg3 > allowance[address(arg1)][address(msg.sender)]:
+        revert with 0, 
+                    32,
+                    40,
+                    0x6545524332303a207472616e7366657220616d6f756e74206578636565647320616c6c6f77616e63,
+                    mem[264 len 24],
+                    mem[312 len 8]
+    if not arg1:
+        revert with 0, 32, 36, 0x7345524332303a20617070726f76652066726f6d20746865207a65726f20616464726573, mem[392 len 28]
+    if not msg.sender:
+        revert with 0, 32, 34, 0x7345524332303a20617070726f766520746f20746865207a65726f20616464726573, mem[390 len 30]
+    allowance[address(arg1)][address(msg.sender)] -= arg3
+    emit Approval((allowance[address(arg1)][address(msg.sender)] - arg3), arg1, msg.sender);
+    return 1
+}
+
+
+
+}
