f1b57f8e790cd1b09371f18b958a93b036c5cf1e
abagoforgans <abagoforgans@users.noreply.github.com>
2022-07-09 22:18:10 +0000

    autocommit

---
+++ contracts/0x34CCc5Bf1135f0cAA2790CBb57C7F7ef6A2003B2.sol
@@ -0,0 +1,71 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+array of struct stor0;
+
+function _fallback() payable {
+    revert
+}
+
+function commitRecord(string arg1, string arg2, uint256 arg3, string arg4, bytes32 arg5) payable {
+    require calldata.size - 4 >= 160
+    require arg1 <= test266151307()
+    require arg1 + 35 < calldata.size
+    require arg1.length <= test266151307()
+    require ceil32(arg1.length) + 128 >= 96 and ceil32(arg1.length) + 128 <= test266151307()
+    require arg1 + arg1.length + 36 <= calldata.size
+    mem[128 len arg1.length] = arg1[all]
+    mem[arg1.length + 128] = 0
+    require arg2 <= test266151307()
+    require arg2 + 35 < calldata.size
+    require arg2.length <= test266151307()
+    require ceil32(arg2.length) + 160 >= 128 and ceil32(arg1.length) + ceil32(arg2.length) + 160 <= test266151307()
+    mem[ceil32(arg1.length) + 128] = arg2.length
+    require arg2 + arg2.length + 36 <= calldata.size
+    mem[ceil32(arg1.length) + 160 len arg2.length] = arg2[all]
+    mem[arg2.length + ceil32(arg1.length) + 160] = 0
+    require arg3 == arg3
+    require arg4 <= test266151307()
+    require arg4 + 35 < calldata.size
+    require arg4.length <= test266151307()
+    require ceil32(arg4.length) + 192 >= 160 and ceil32(arg1.length) + ceil32(arg2.length) + ceil32(arg4.length) + 192 <= test266151307()
+    mem[ceil32(arg1.length) + ceil32(arg2.length) + 160] = arg4.length
+    require arg4 + arg4.length + 36 <= calldata.size
+    mem[ceil32(arg1.length) + ceil32(arg2.length) + 192 len arg4.length] = arg4[all]
+    mem[arg4.length + ceil32(arg1.length) + ceil32(arg2.length) + 192] = 0
+    require arg5 == arg5
+    mem[ceil32(arg1.length) + ceil32(arg2.length) + ceil32(arg4.length) + 192] = 96
+    mem[ceil32(arg1.length) + ceil32(arg2.length) + ceil32(arg4.length) + 224] = 0
+    mem[ceil32(arg1.length) + ceil32(arg2.length) + ceil32(arg4.length) + 256] = ceil32(arg1.length) + 128
+    mem[ceil32(arg1.length) + ceil32(arg2.length) + ceil32(arg4.length) + 288] = block.timestamp
+    stor0[arg5].field_0++
+    stor0[arg5][stor0[arg5].field_0][].field_0 = Array(len=Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)], data=mem[ceil32(arg1.length) + 160 len Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)]])
+    stor0[arg5][stor0[arg5].field_0].field_256 = block.timestamp
+    mem[ceil32(arg1.length) + ceil32(arg2.length) + ceil32(arg4.length) + 320] = arg5
+    mem[ceil32(arg1.length) + ceil32(arg2.length) + ceil32(arg4.length) + 352] = 160
+    mem[ceil32(arg1.length) + ceil32(arg2.length) + ceil32(arg4.length) + 480] = arg1.length
+    mem[ceil32(arg1.length) + ceil32(arg2.length) + ceil32(arg4.length) + 512 len ceil32(arg1.length)] = arg1[all], mem[arg1.length + 128 len ceil32(arg1.length) - arg1.length]
+    if ceil32(arg1.length) > arg1.length:
+        mem[ceil32(arg1.length) + ceil32(arg2.length) + ceil32(arg4.length) + arg1.length + 512] = 0
+    mem[ceil32(arg1.length) + ceil32(arg2.length) + ceil32(arg4.length) + 384] = ceil32(arg1.length) + 192
+    mem[(2 * ceil32(arg1.length)) + ceil32(arg2.length) + ceil32(arg4.length) + 512] = Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)]
+    mem[(2 * ceil32(arg1.length)) + ceil32(arg2.length) + ceil32(arg4.length) + 544 len ceil32(Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)])] = mem[ceil32(arg1.length) + 160 len ceil32(Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)])]
+    if ceil32(Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)]) > Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)]:
+        mem[(2 * ceil32(arg1.length)) + ceil32(arg2.length) + ceil32(arg4.length) + Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)] + 544] = 0
+    mem[ceil32(arg1.length) + ceil32(arg2.length) + ceil32(arg4.length) + 416] = arg3
+    mem[ceil32(arg1.length) + ceil32(arg2.length) + ceil32(arg4.length) + 448] = ceil32(arg1.length) + ceil32(Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)]) + 224
+    mem[(2 * ceil32(arg1.length)) + ceil32(arg2.length) + ceil32(arg4.length) + ceil32(Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)]) + 544] = Mask(8 * -ceil32(arg2.length) + arg2.length + 32, 0, 0), mem[ceil32(arg1.length) + arg2.length + 192 len -arg2.length + ceil32(arg2.length)]
+    mem[(2 * ceil32(arg1.length)) + ceil32(arg2.length) + ceil32(arg4.length) + ceil32(Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)]) + 576 len ceil32(Mask(8 * -ceil32(arg2.length) + arg2.length + 32, 0, 0), mem[ceil32(arg1.length) + arg2.length + 192 len -arg2.length + ceil32(arg2.length)])] = mem[ceil32(arg1.length) + ceil32(arg2.length) + 192 len ceil32(Mask(8 * -ceil32(arg2.length) + arg2.length + 32, 0, 0), mem[ceil32(arg1.length) + arg2.length + 192 len -arg2.length + ceil32(arg2.length)])]
+    if ceil32(Mask(8 * -ceil32(arg2.length) + arg2.length + 32, 0, 0), mem[ceil32(arg1.length) + arg2.length + 192 len -arg2.length + ceil32(arg2.length)]) > Mask(8 * -ceil32(arg2.length) + arg2.length + 32, 0, 0), mem[ceil32(arg1.length) + arg2.length + 192 len -arg2.length + ceil32(arg2.length)]:
+        mem[(2 * ceil32(arg1.length)) + ceil32(arg2.length) + ceil32(arg4.length) + ceil32(Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)]) + Mask(8 * -ceil32(arg2.length) + arg2.length + 32, 0, 0), mem[ceil32(arg1.length) + arg2.length + 192 len -arg2.length + ceil32(arg2.length)] + 576] = 0
+    emit 0x995375de: mem[ceil32(arg1.length) + ceil32(arg2.length) + ceil32(arg4.length) + 320 len ceil32(arg1.length) + ceil32(Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)]) + ceil32(Mask(8 * -ceil32(arg2.length) + arg2.length + 32, 0, 0), mem[ceil32(arg1.length) + arg2.length + 192 len -arg2.length + ceil32(arg2.length)]) + 256], block.timestamp
+}
+
+
+
+}
